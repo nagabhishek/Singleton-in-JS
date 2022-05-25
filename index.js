74a70df8627a1d78dd82dbbabaff284a647f5cb0
nagabhishek <nag.abhishek2012@gmail.com>
2022-05-25 13:54:57 +0530

    1 file modified

--- index.js
+++ index.js
@@ -4,3 +4,25 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+const Singleton = (function () {
+  let instance;
+
+  function createObject() {
+    return new Object('Hello!');
+  }
+
+  return {
+    getInstance: function () {
+      if (!instance) {
+        instance = createObject();
+      }
+      return instance;
+    },
+  };
+})();
+
+const i1 = Singleton.getInstance();
+const i2 = Singleton.getInstance();
+
+console.log('is both instance same:', i1 === i2);
